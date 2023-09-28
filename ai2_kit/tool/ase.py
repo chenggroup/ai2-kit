0b22c234d227b959c4e6bc1a1612bb00c6aa3008
weihong.xu <xuweihong.cn@gmail.com>
2023-09-28 11:16:27 +0800

    feat: ase tool support write cp2k-inc

--- ai2_kit/tool/ase.py
+++ ai2_kit/tool/ase.py
@@ -2,6 +2,7 @@ import ase.io
 import glob
 
 from ai2_kit.core.util import ensure_dir
+from ai2_kit.domain.cp2k import dump_coord_n_cell
 from typing import List, Union
 from ase import Atoms
 
@@ -53,11 +54,19 @@ class AseHelper:
         self._atoms_list += data
 
     def _write(self, filename: str, atoms_list, **kwargs):
+        assert len(atoms_list) > 0, 'No atoms to write'
         ensure_dir(filename)
         fmt = kwargs.get('format', None)
         if fmt == 'lammps-dump-text':
             return self._write_lammps_dump_text(filename, atoms_list, **kwargs)
+        if fmt == 'cp2k-inc' or (fmt is None and filename.endswith('.inc')):
+            assert len(atoms_list) == 1, 'cp2k-inc only support single frame'
+            return self._write_cp2k_inc(filename, atoms_list[0])
         return ase.io.write(filename, atoms_list, **kwargs)
+
+    def _write_cp2k_inc(self, filename: str, atoms: Atoms):
+        with open(filename, 'w') as f:
+            dump_coord_n_cell(f, atoms)
 
     def _write_lammps_dump_text(self, filename: str, atoms_list, **kwargs):
         if not isinstance(atoms_list, list):

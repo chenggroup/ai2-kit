61f904d64136850e91d5159fb92d6c64ec532bdd
weihong.xu <xuweihong.cn@gmail.com>
2023-11-22 15:39:19 +0800

    dpdata tool support write fparam.npy

--- ai2_kit/tool/dpdata.py
+++ ai2_kit/tool/dpdata.py
@@ -1,23 +1,26 @@
 from ai2_kit.core.util import ensure_dir
 from ai2_kit.core.log import get_logger
 
+import numpy as np
 import dpdata
 import glob
+import os
 
 logger = get_logger(__name__)
 
 class DpdataHelper:
 
     def __init__(self, label: bool = True):
+        """
+        label: if True, read data with labels (force, energy, etc), else read data without labels,
+          use --nolabel to disable reading labels
+        """
         self._systems = []
         self._label = label
 
-    def read(self, *file_path_or_glob: str,**kwargs):
+    def read(self, *file_path_or_glob: str, **kwargs):
         kwargs.setdefault('fmt', 'deepmd/npy')
-        files = []
-        for file_path in file_path_or_glob:
-            files += sorted(glob.glob(file_path, recursive=True)) if '*' in file_path else [file_path]
-
+        files = _expand_paths(*file_path_or_glob)
         if len(files) == 0:
             raise FileNotFoundError(f'No file found for {file_path_or_glob}')
         for file in files:
@@ -38,7 +41,6 @@ class DpdataHelper:
         ensure_dir(out_path)
         if len(self._systems) == 0:
             raise ValueError('No data to merge')
-
         if merge:
             systems = dpdata.MultiSystems(self._systems[0])
         else:
@@ -56,8 +58,44 @@ class DpdataHelper:
         else:
             raise ValueError(f'Unknown fmt {fmt}')
 
+    def write_fparams(self, *file_path_or_glob: str, fparams):
+        """
+        write fparam.npy to dataset, only support deepmd/npy format.
+
+        This command will search all *.npy files under the specific path
+        and create a fparam.npy file next to it.
+
+        :param file_path_or_glob: path or glob pattern to find *.npy files
+        :param fparams: fparams to write, can be a single value or a list of values
+        """
+        write_fparams(*file_path_or_glob, fparams=fparams)
+
+
     def _read(self, file: str, **kwargs):
         if self._label:
             self._systems.extend(dpdata.LabeledSystem(file, **kwargs))
         else:
             self._systems.extend(dpdata.System(file, **kwargs))
+
+
+def write_fparams(*file_path_or_glob: str, fparams):
+    # search for box.npy files
+    paths = _expand_paths(*[os.path.join(path, '**/box.npy')
+                            for path in file_path_or_glob])
+    if len(paths) == 0:
+        raise FileNotFoundError(f'No deepmd/npy datasets found in {file_path_or_glob}')
+
+    for path in paths:
+        box_arr = np.load(path)
+        logger.debug(f'box.npy shape: {box_arr.shape}')
+        fparam_arr = np.tile(fparams, (len(box_arr), 1))
+        logger.debug(f'fparam.npy shape: {fparam_arr.shape}')
+        fparam_file = os.path.join(os.path.dirname(path), 'fparam.npy')
+        np.save(fparam_file, fparam_arr)
+
+
+def _expand_paths(*file_path_or_glob: str):
+    files = []
+    for file_path in file_path_or_glob:
+        files += sorted(glob.glob(file_path, recursive=True)) if '*' in file_path else [file_path]
+    return files

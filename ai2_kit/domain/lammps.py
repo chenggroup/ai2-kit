127d4fa072f34e4f7b2b051add12a830b1932c64
weihong.xu <xuweihong.cn@gmail.com>
2024-02-23 12:02:15 +0800

    support relative path

--- ai2_kit/domain/lammps.py
+++ ai2_kit/domain/lammps.py
@@ -329,6 +329,7 @@ def make_lammps_task_dirs(combination_vars: Mapping[str, Sequence[Any]],
                             dp_sel_type: Optional[List[int]],
                             mode: TRAINING_MODE,
                             ai2_kit_cmd: str,
+                            rel_path_to: Optional[str] = None,  # For dflow-galaxy
                             ):
     # setup workspace
     input_data_dir = os.path.join(work_dir, 'input_data')
@@ -412,7 +413,11 @@ def make_lammps_task_dirs(combination_vars: Mapping[str, Sequence[Any]],
                                                         type_alias=type_alias, sel_type=dp_sel_type)
 
         ## build variables section
-        lammps_vars['DATA_FILE'] = data_file['url']
+        if rel_path_to:
+            lammps_vars['DATA_FILE'] = os.path.relpath(data_file['url'], rel_path_to)
+        else:
+            lammps_vars['DATA_FILE'] = data_file['url']
+
         lammps_vars['N_STEPS'] = n_steps
         lammps_vars['THERMO_FREQ'] = sample_freq
         lammps_vars['DUMP_FREQ'] = sample_freq

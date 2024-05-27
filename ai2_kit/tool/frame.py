9d6cb626b6e0e7d50f7a94428e02d4fe9618509f
weihong.xu <xuweihong.cn@gmail.com>
2024-05-27 09:49:30 +0800

    refactor frame tool

--- ai2_kit/tool/frame.py
+++ ai2_kit/tool/frame.py
@@ -1,6 +1,8 @@
+from typing import List
+import re
+
 from ai2_kit.core.util import expand_globs, slice_from_str, SAMPLE_METHOD, list_sample, ensure_dir
 from ai2_kit.core.log import get_logger
-import re
 
 
 logger = get_logger(__name__)
@@ -27,26 +29,16 @@ class FrameTool:
         :param frame_size: line number of each frame
         :param rp: repeated pattern, can be string or regex, e.g. 'ITEM: TIMESTEP', 'Lattice.+'
         """
-        files = expand_globs(path_or_glob)
-        if len(files) == 0:
-            raise FileNotFoundError(f'No file found in {path_or_glob}')
-        all_lines = []
-        for file in files:
-            with open(file) as f:
-                lines = f.readlines()
-                if header_size > 0:
-                    self.header = lines[:header_size]
-                    lines = lines[header_size:]
-                all_lines.extend(lines)
+        header, frames = load_frames(*path_or_glob, frame_size=frame_size, rp=rp, header_size=header_size)
 
-        if frame_size <= 0:
-            if rp is None:
-                raise ValueError('either frame_size or rp (repeat pattern) should be set')
-            frame_size = detect_frame_size(all_lines, rp)
+        if self.header and header and self.header != header:
+            logger.warning('Header is different from previous loaded data')
+            logger.info(f'Previous header: {''.join(self.header)}')
+            logger.info(f'Current header: {''.join(header)}')
 
-        if len(all_lines) % frame_size > 0:
-            raise ValueError(f'Invalid frame lines {frame_size}, cannot divide {len(all_lines)} lines into frames')
-        self.frames.extend(all_lines[i: i + frame_size] for i in range(0, len(all_lines), frame_size))
+        self.header = header
+        self.frames.extend(frames)
+
         return self
 
     def slice(self, expr: str):
@@ -92,16 +84,60 @@ class FrameTool:
                 f.writelines(frame)
 
 
-def detect_frame_size(l: list, rp: str, max_lines = 4096):
+def load_frames(*path_or_glob: str, frame_size: int = 0, rp = None, header_size: int = 0):
+    """
+    Load frames from multiple files
+
+    :param path_or_glob: path or glob pattern to locate data path
+    :param frame_size: line number of each frame
+    :param rp: repeated pattern, can be string or regex, e.g. 'ITEM: TIMESTEP', 'Lattice.+'
+    """
+    files = expand_globs(path_or_glob, raise_invalid=True)
+    if len(files) == 0:
+        raise FileNotFoundError(f'No file found in {path_or_glob}')
+
+    header = []
+    all_frames = []
+    for file in files:
+        with open(file) as f:
+            lines = f.readlines()
+            header, frames = parse_frames(lines, frame_size, rp, header_size)
+            all_frames.extend(frames)
+    return header, all_frames
+
+
+def parse_frames(lines: List[str], frame_size: int = 0, rp = None, header_size: int = 0):
+    """
+    parse frames from lines
+
+    :param lines: lines of data
+    :param frame_size: line number of each frame
+    :param rp: repeated pattern, can be string or regex, e.g. 'ITEM: TIMESTEP', 'Lattice.+'
+    :param header_size: size of header lines
+    """
+    if header_size > 0:
+        header = lines[:header_size]
+        lines = lines[header_size:]
+    else:
+        header = []
+    if frame_size <= 0:
+        if rp is None:
+            raise ValueError('either frame_size or rp (repeat pattern) should be set')
+        frame_size = detect_frame_size(lines, rp)
+
+    if len(lines) % frame_size > 0:
+        raise ValueError(f'Invalid frame lines {frame_size}, cannot divide {len(lines)} lines into frames')
+    return header, [lines[i: i + frame_size] for i in range(0, len(lines), frame_size)]
+
+
+def detect_frame_size(l: list, rp: str):
     """
     detect frame size of a file by repeating pattern
-    :param rp: repeated pattern, can be string or regex, e.g. 'ITEM: TIMESTEP', 'Lattice.+'
+    :param rp: repeated pattern, can be string or regex, e.g. 'TIMESTEP' (for lammpstrj), 'Lattice' (for xyz)
     """
     pattern = re.compile(rp)
     lno = -1
     for i, line in enumerate(l):
-        if i > max_lines:
-            break
         if pattern.search(line):
             logger.info(f'Detected pattern: {line} at line {i} by pattern {rp}')
             if lno < 0:
@@ -110,4 +146,4 @@ def detect_frame_size(l: list, rp: str, max_lines = 4096):
                 ret = i - lno
                 logger.info(f'Detected frame lines: {ret}')
                 return ret
-    raise ValueError(f'Cannot detect frame lines in {max_lines} lines')
+    raise ValueError(f'Cannot detect frame lines in {len(l)} lines')

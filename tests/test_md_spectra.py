ec4706069ddefb8e9be4af9d9499e0008f8ac758
nagoriyuki <nagoriyukigrind@outlook.com>
2025-06-19 10:11:32 +0800

    Add:Added empty test scripts and data for md_spectra

---
+++ tests/test_md_spectra.py
@@ -0,0 +1,10 @@
+from pathlib import Path
+import unittest
+
+import numpy as np
+import dpdata
+
+data_dir = Path(__file__).parent / 'data-sample'
+
+if __name__ == '__main__':
+    unittest.main()

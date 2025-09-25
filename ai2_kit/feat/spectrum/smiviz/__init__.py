f99e75b72b02269070fff54b08ab935015ff986b
RinRingun <117254290+RinRingun@users.noreply.github.com>
2025-09-25 17:29:54 +0800

    Notebook for redox potential
    Co-authored-by: nagoriyuki <nagoriyukigrind@outlook.com>

---
+++ ai2_kit/feat/spectrum/smiviz/__init__.py
@@ -0,0 +1,7 @@
+from .gapplot import (
+    plot_energy_force_error,
+    plot_ti_results,
+    plot_vertical_energy_gaps,
+    plot_smoothed_vertical_energy_gaps,
+    plot_thermodynamic_integration,
+)
